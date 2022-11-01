3ac965cf82c83ba91a221ba6b19874d9fe75abc7
Fabricio M <fmenegolo@hotmail.com>
2022-11-01 13:48:29 -0300

    Update CodePark_3-3_contornarProblemas.py

--- 2_Introducao_programacao/CodePark_3-3_contornarProblemas.py
+++ 2_Introducao_programacao/CodePark_3-3_contornarProblemas.py
@@ -4,3 +4,25 @@
 
 #Caso o usuário não digite um número ou apareça um inválido no campo do ano, o sistema informará o erro
 #  e continuará perguntando até que um valor correto seja preenchido.
+
+
+def quatosAnos(anoNasc,anoAtual):
+    anoAtual=2022
+    if (anoNasc >= anoAtual):
+        raise Exception("ano de nascimento maior qu o atual")
+    elif (anoNasc >= 1922) and (anoNasc < anoAtual):
+        idade=anoAtual-anoNasc
+    else:
+        raise Exception("")
+    return idade
+rodar = True
+while(rodar == True)
+    nome = input("insira seu nome: ")
+    nascidoEm = int(input("Digite o seu ano de nascimento: "))
+    anoHoje = 0
+    try:
+        resp = quatosAnos(nascidoEm, anoHoje)
+        print(resp)
+        rodar = False
+    except:
+         print("Caracter inválido, por favor digite um número par")
