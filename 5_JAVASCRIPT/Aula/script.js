7a6be5173063f73377ad34947bdcafc1c8300806
Fabricio M <fmenegolo@hotmail.com>
2023-01-10 20:56:22 -0300

    Aula10JAN23

---
+++ 5_JAVASCRIPT/Aula/script.js
@@ -0,0 +1,4 @@
+const linkPerfil = document.getElementById('link perfil')
+const navPerfil = document.getElementById('nav perfil')
+const linkDados = document.getElementById('link-perfil-dados')
+const voltar = document.getElementById('voltar')
