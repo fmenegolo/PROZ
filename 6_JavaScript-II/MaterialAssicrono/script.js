4c759a7df24c7ccd6bf2d2bfb3650eaa1e08fdf2
Fabricio M <fmenegolo@hotmail.com>
2023-01-24 21:49:48 -0300

    modulo2 JS-II

--- 6_JavaScript-II/MaterialAssicrono/script.js
+++ 6_JavaScript-II/MaterialAssicrono/script.js
@@ -28,3 +28,67 @@ section.classList.remove("borda-azul"); //metodo remove() passando o nome do arg
 //Alternando classes "metodo toggle()"
 section.classList.toggle("borda-azul");
 section.classList.toggle("borda-azul");// ele verifica se o metodo tem a classe e conforme o resultado ele adiciona ou remove o mesmo.
+
+//trabalhar com eventos
+let numero = document.querySelector("h1"); // capturar elemento h1, que exibira um número.
+let botaoMais = document.querySelector(".botao-mais"); // elemento button, que ativara o comportamento.
+let contagem = 0; // declarar variavél de contagem
+
+function adicionarUm(){
+    contagem = contagem + 1;
+    numero.innerText = contagem;
+}; // definir comportamento por uma função, depois exibir o valor via innerText.
+
+adicionarUm();// aparecera 1 ao invez de 0
+botaoMais.addEventListener("click", adicionarUm); // função para executar um evento + dois argumentos 1º String que define o tipo de evento 2º é a função ou comportamento a ser executado.
+
+
+// Uma alternativa, escrevê-la como uma função anônima dentro do parênteses do addEventListener.
+let botaoMenos = document.querySelector(".botao-menos");
+botaoMenos.addEventListener("click", function(){
+
+  contagem = contagem - 1;
+
+  numero.innerText = contagem;
+
+});
+
+//Também há o uso de arrow functions para declarar a função anônima, substituindo o trecho function( ) por ( ) =>
+let botaoLimpar = document.querySelector(".botao-limpar");
+botaoLimpar.addEventListener("click", () => {
+
+  contagem = 0;
+
+  numero.innerText = contagem;
+
+});
+
+//Eventos de Mouse
+let botaoPassou = document.querySelector(".botao-passou");
+let span = document.querySelector("span");
+let section2 = document.querySelector(".section2");
+
+//Evento mouseover é diparado quando o mouse passa por cima
+function mostrarSpan(){
+    span.style.opacity = "100"
+}; // Criar uma função que mude a propriedade do elemento span
+
+botaoPassou.addEventListener("mouseover", mostrarSpan);// declarando o evento e a função
+
+// função ocultar quando o mouse sair
+function ocultarSpan(){
+    span.style.opacity = "0"
+};
+botaoPassou.addEventListener("mouseout", ocultarSpan)
+
+//Evento click
+function fazerUmClick(){
+    section2.innerText = "Fez um click simples"
+};
+botaoPassou.addEventListener("click", fazerUmClick);
+
+// Evento click duplo
+function fazerDoisClicks(){
+    section2.innerText = "Fez duplo click!"
+};
+botaoPassou.addEventListener("dblclick", fazerDoisClicks);
